8aae9f6c3c7fa9f5994e39e313d9176a83382358
drzobin <me@drz.se>
2025-05-08 20:51:08 +0200

    Adding logging

--- src/ddmail_e2e_testing/__main__.py
+++ src/ddmail_e2e_testing/__main__.py
@@ -1,8 +1,10 @@
 import sys
 import os
 import argparse
+import logging
 import toml
 from ddmail_e2e_testing.test_auth import test_register, test_login_logout
+from ddmail_e2e_testing.test_email import test_add_email
 
 def main():
     # Get arguments from args.
@@ -19,11 +21,35 @@ def main():
     with open(args.config_file, 'r') as f:
         toml_config = toml.load(f)
 
+    # Setup logging.
+    logger = logging.getLogger(__name__)
+    console_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler("app.log", mode="a", encoding="utf-8")
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    formatter = logging.Formatter(
+        "{asctime} {levelname} in {module} {funcName} {lineno}: {message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M",
+        )
+
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    logger.setLevel(logging.DEBUG)
+
     # Testing register.
-    test_register(toml_config)
+    logger.info("Running test_register")
+    test_register(toml_config,logger)
 
     # Test login and logout.
-    test_login_logout(toml_config)
+    logger.info("Running test_login_logout")
+    test_login_logout(toml_config,logger)
+
+    # Test to add an email account
+    logger.info("Running test_add_email")
+    test_add_email(toml_config,logger)
 
 if __name__ == "__main__":
     main()
