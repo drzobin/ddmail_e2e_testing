0d04d136541fc12346355021f74f7bcc3a77ba9b
drzobin <me@drz.se>
2025-05-08 20:51:18 +0200

    Adding logging

--- src/ddmail_e2e_testing/test_auth.py
+++ src/ddmail_e2e_testing/test_auth.py
@@ -3,14 +3,22 @@ import toml
 import requests
 import ddmail_e2e_testing.helpers as helpers
 
-def test_register(toml_config):
-    print("Testing /register")
+def test_register(toml_config, logger):
     # Use requests session to easy get out auth cookie to follow along our requests.
     s = requests.Session()
 
     # Get /register csrf token.
     register_url = toml_config["URL"] + "/register"
     response = s.get(register_url, timeout=1)
+
+    # Check that we get status code 200.
+    if response.status_code != 200:
+        msg = "GET " + register_url + " did not returned status code 200"
+        logger.error(msg)
+
+        return msg
+
+    # Parse the csrf token from html content.
     csrf_token = helpers.get_csrf_token(response.content)
 
     # Register new account and user.
@@ -31,45 +39,72 @@ def test_register(toml_config):
 
     response = s.post(login_url, data=data, files=files, timeout=2)
 
-    # Check if login workes with the newly registered/created account/user/password and key.
-    if response.status_code == 200 and "Logged in as user: " + auth_data["username"] in str(response.content):
-        print("working")
-        return "working"
-    else:
-        print("fail")
-        return "fail"
+    # Check if POST /login returns status code 200.
+    if response.status_code != 200:
+        msg = "POST " + login_url + " did not returned status code 200"
+        logger.error(msg)
 
-def test_login_logout(toml_config):
-    print("Testing /login")
+        return msg
+
+    # Check that POST /login
+    if "Logged in as user: " + auth_data["username"] not in str(response.content):
+        msg = "POST " + login_url + " did not returned correct content"
+        logger.error(msg)
+
+        return msg
+
+    # All is working.
+    logger.info("working")
+    return "working"
+
+
+def test_login_logout(toml_config, logger):
+    print("Testing /login and /logout")
     main_url = toml_config["URL"] 
     logout_url = toml_config["URL"] + "/logout"
 
     s = helpers.login(toml_config)
+    
+    # Check that login worked.
+    if s == None:
+        msg = "login failed"
+        logger.error(msg)
+
+        return msg
+
     response = s.get(main_url, timeout=1)
 
     # Check if login worked and returned status code 200.
     if response.status_code != 200:
-        print("fail")
-        return "fail"
+        msg = "GET " + main_url + " did not returned status code 200"
+        logger.error(msg)
+
+        return msg
     
     # Check if login worked.
     if "Logged in as user: " + toml_config["TEST_ACCOUNT"]["USERNAME"] not in str(response.content):
-        print("fail")
-        return "fail"
+        msg = "GET " + main_url + " did not returned correct content"
+        logger.error(msg)
+
+        return msg
 
     # Logout.
     response = s.get(logout_url, timeout=1)
     
     # Check if logout worked and returned status code 200.
     if response.status_code != 200:
-        print("fail")
-        return "fail"
+        msg = "GET " + logout_url + " did not returned status code 200"
+        logger.error(msg)
+
+        return msg
     
     # Check if logout worked.
-    if "Logged in as user: Not logged in" in str(response.content):
-        print("working")
-        return "working"
-    else:
-        print("fail")
-        return "fail"
+    if "Logged in as user: Not logged in" not in str(response.content):
+        msg = "fail: GET " + logout_url + " did not returned correct content"
+        logger.error(msg)
+
+        return msg
+
+    logger.info("working")
+    return "working"
 
